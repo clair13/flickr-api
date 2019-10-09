6c803fcb0cb4d74a9c06df4409e9b40c8b237716
clair13 <theclairvoyant1313@yahoo.gr>
2019-10-09 20:34:29 +0300

    Configure figaro and flickr object gems and create static pages controller

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,8 @@ module FlickrApi
     # the framework and any gems in your application.
   end
 end
+
+Flickr.configure do |config|
+  config.api_key       = ENV['flickr_api_key']
+  config.shared_secret = ENV['flickr_api_secret']
+end
