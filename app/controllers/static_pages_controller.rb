42734234789f5b8518b71c7e5b88e6d89f70ffa9
clair13 <theclairvoyant1313@yahoo.gr>
2019-10-13 17:09:55 +0300

    Change styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   end
   def photos
+    @person = Flickr.people.find(params[:search][:id])
+    @photos =  Flickr.photos.search(user_id: @person.id)
   end
 end
