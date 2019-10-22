2872012f5d23c123747c432af2bdc65bc472f900
clair13 <theclairvoyant1313@yahoo.gr>
2019-10-22 19:58:15 +0300

    Fix issue with image_tag helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
   def photos
     @person = Flickr.people.find(params[:search][:id])
     @photos =  Flickr.photos.search(user_id: @person.id)
